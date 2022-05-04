0450f3d5e8b2378f38d1f97770d04e715a53fdcd
Daniel Centore <daniel.centore@gmail.com>
2022-05-03 22:30:32 -0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { UppyNextS3MultipartEndpoint } from "./UppyNextS3MultipartEndpoint";
+import { getUppyNextS3MultipartOptions } from "./UppyNextS3MultipartOptions";
+
+export { UppyNextS3MultipartEndpoint, getUppyNextS3MultipartOptions }
